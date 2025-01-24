48a8af9f4310fe0bf85eafaa1dbdce7b7cf64207
Zeeshan Ahmad <itexpert120@outlook.com>
2025-01-24 23:49:33 +0500

    WORKING

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi.responses import JSONResponse
 import numpy as np
 import pandas as pd
 from tensorflow.keras.applications import VGG16
@@ -18,12 +19,20 @@ try:
     # Load embeddings and styles data
     embedding_path = "./df_embeddings.csv"
     styles_path = "./styles.csv"
+    images_path = "./images.csv"
 
     df_embeddings = pd.read_csv(embedding_path, index_col=0)
     embeddings_array = df_embeddings.values
 
     styles_df = pd.read_csv(styles_path, nrows=6000, on_bad_lines="skip")
     styles_df["image"] = styles_df["id"].apply(lambda x: f"{x}.jpg")
+
+    images_df = pd.read_csv(images_path, nrows=6000, on_bad_lines="skip")
+
+    # add the link column from images_df to styles_df using the filename column
+    styles_df["link"] = styles_df["image"].map(
+        lambda x: images_df[images_df["filename"] == x]["link"].values[0]
+    )
 
     # Load and configure VGG16 model
     vgg16 = VGG16(include_top=False, weights="imagenet", input_shape=(100, 100, 3))
@@ -52,12 +61,18 @@ def generate_recommendations(image_path: str) -> list:
     sorted_indices = sorted(
         enumerate(norm_similarity[0]), key=lambda x: x[1], reverse=True
     )[:5]
-    return styles_df["image"].iloc[[i[0] for i in sorted_indices]].tolist()
+    recommended_ids = styles_df["image"].iloc[[i[0] for i in sorted_indices]].tolist()
+
+    # Create a mapping from filename to link
+    filename_to_link = dict(zip(images_df["filename"], images_df["link"]))
+    # Get the corresponding links in order
+    recommended_links = [filename_to_link[filename] for filename in recommended_ids]
+
+    return recommended_links
 
 
 @app.post("/recommendations/")
 async def get_recommendations(file: UploadFile = File(...)):
-    print("here")
     if not file.content_type.startswith("image/"):
         raise HTTPException(400, "Uploaded file must be an image")
 
@@ -77,6 +92,26 @@ async def get_recommendations(file: UploadFile = File(...)):
 
     finally:
         os.remove(temp_path)
+
+
+@app.get("/items")
+async def get_items(page: int = 1, page_size: int = 10):
+    """Get a list of items."""
+    return {
+        "items": styles_df.iloc[page * page_size : (page + 1) * page_size].to_dict(
+            orient="records"
+        ),
+        "total": styles_df.shape[0],
+        "page": page,
+        "page_size": page_size,
+        "pages": styles_df.shape[0] // page_size + 1,
+        "has_next": page < styles_df.shape[0] // page_size,
+        "has_prev": page > 1,
+        "next": page + 1 if page < styles_df.shape[0] // page_size else None,
+        "prev": page - 1 if page > 1 else None,
+        "first": 1,
+        "last": styles_df.shape[0] // page_size + 1,
+    }
 
 
 # For debugging purposes
